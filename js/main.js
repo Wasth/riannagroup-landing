fc7e10c1e81e944fc23fdfb8f90bea6a2a944d30
Rick <riasta@yandex.ru>
2018-05-11 00:45:32 +0300

    realized slider engine

--- js/main.js
+++ js/main.js
@@ -2,21 +2,71 @@ $(function () {
 
     // kind of class for slider
     function Slider(content, slidingTime) {
+        // declarations
         this.sliderBlock = content;
         this.slidingTime = slidingTime;
         this.sliderI = 0;
         this.slideCount = $(this.sliderBlock+' .slides .slide').length;
-        alert()
-        var plusI = function () {
 
+        this.plusI = function () {
+            if (this.sliderI === this.slideCount - 1){
+                this.sliderI = 0;
+            }else {
+                this.sliderI += 1;
+            }
+        };
+        this.minusI = function () {
+            if (this.sliderI === 0){
+                this.sliderI = this.slideCount - 1;
+            } else {
+                this.sliderI -= 1;
+            }
+        };
+        this.getI = function (i) {
+            return $(this.sliderBlock+' .slides .slide:eq('+i+')');
+        };
+        this.setItoLeft = function (i, time) {
+            this.getI(i).animate({
+                'left':'-100%'
+            },time);
+        };
+        this.setItoRight = function (i, time) {
+            this.getI(i).animate({
+                'left':'100%'
+            },time);
+        };
+        this.setItoCenter = function (i, time) {
+            this.getI(i).animate({
+                'left':'0'
+            },time);
         }
-        var minusI = function () {
-
+        // preparing slider
+        for(var i = 0;i < this.slideCount;i++) {
+            this.setItoRight(i,0);
         }
-
+        this.setItoCenter(0,0);
+        // arrow click handler
+        $(this.sliderBlock+' .arrow').click({ obj: this }, function (e) {
+            var obj = e.data.obj;
+            console.log(obj);
+            if ($(this).hasClass('left')){
+                obj.setItoLeft(obj.sliderI, obj.slidingTime);
+                obj.setItoRight(obj.sliderI, 0);
+                obj.minusI();
+                obj.setItoCenter(obj.sliderI, obj.slidingTime);
+            }else if($(this).hasClass('right')){
+                obj.setItoRight(obj.sliderI, obj.slidingTime);
+                obj.plusI();
+                obj.setItoLeft(obj.sliderI, 0);
+                obj.setItoCenter(obj.sliderI, obj.slidingTime);
+            }
+        });
     }
-    var slidingTime = 300
-    var sliderOpora = new Slider('#oporaSlider .slider-content', slidingTime);
-    // var sliderScheme = new Slider('#schemeSlider .slider-content', slidingTime);
-    // var sliderProject = new Slider('#ourProjects .slider-content', slidingTime);
-})
+    var slidingTime = 300;
+    var sliderOpora1 = new Slider('#oporaSlider .slider-content:eq(0)', slidingTime);
+    var sliderOpora2 = new Slider('#oporaSlider .slider-content:eq(1)', slidingTime);
+    var sliderOpora3 = new Slider('#oporaSlider .slider-content:eq(2)', slidingTime);
+    var sliderOpora4 = new Slider('#oporaSlider .slider-content:eq(3)', slidingTime);
+    var sliderScheme = new Slider('#schemeSlider .slider-content', slidingTime);
+    var sliderProject = new Slider('#ourProjects .slider-content', slidingTime);
+});
