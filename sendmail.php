9776bd519b5fcf58aff0df009c9728e5a9fd2a7b
Rick <riasta@yandex.ru>
2018-05-12 23:21:24 +0300

    sendmail function another fix

--- sendmail.php
+++ sendmail.php
@@ -20,7 +20,7 @@ $body .= "Content-Transfer-Encoding: quoted-printablenn";
 $body .= $message."\n";
 $body .= "--$boundary\n";
 //var_dump($_FILES);
-if(isset($_FILES['order'])) {
+if(!empty($_FILES['order']['name'])) {
     $file = fopen($_FILES['order']['tmp_name'], "r"); //Открываем файл
     $text = fread($file, filesize($_FILES['order']['tmp_name'])); //Считываем весь файл
     fclose($file); //Закрываем файл
@@ -29,9 +29,9 @@ if(isset($_FILES['order'])) {
     $body .= "Content-Transfer-Encoding: base64\n";
     $body .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode($_FILES['order']['name'])."?=\n\n";
     $body .= chunk_split(base64_encode($text))."\n";
-    $body .= "---".$boundary ."--\n";
+//    $body .= "--$boundary\n";
 }
-if(isset($_FILES['rekv'])) {
+if(!empty($_FILES['rekv']['name'])) {
     $file = fopen($_FILES['rekv']['tmp_name'], "r"); //Открываем файл
     $text = fread($file, filesize($_FILES['rekv']['tmp_name'])); //Считываем весь файл
     fclose($file); //Закрываем файл
@@ -40,7 +40,7 @@ if(isset($_FILES['rekv'])) {
     $body .= "Content-Transfer-Encoding: base64\n";
     $body .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode($_FILES['rekv']['name'])."?=\n\n";
     $body .= chunk_split(base64_encode($text))."\n";
-    $body .= "---".$boundary ."--\n";
+//    $body .= "--$boundary\n";
 }
 $body .= "---".$boundary ."--\n";
 mail($to, $subject, $body, $headers); //Отправляем письмо
